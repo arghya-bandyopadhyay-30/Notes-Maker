a43c84c931d6d7a88c5216da476032caa6528075
Arghya Bandyopadhyay <arghya.bandyopadhyay.official1@gmail.com>
2026-08-11 10:49:50 +0530

    Add FileSystem.is_dir method

--- utils/file_system.py
+++ utils/file_system.py
@@ -10,6 +10,9 @@ class FileSystem:
     def is_file(self, path: str) -> bool:
         return os.path.isfile(path)
 
+    def is_dir(self, path: str) -> bool:
+        return os.path.isdir(path)
+
     def read_yaml(self, path: str) -> dict:
         if not self.is_file(path):
             raise FileNotFoundError(f"File not found: {path}")
@@ -28,7 +31,7 @@ class FileSystem:
         return data
 
     def ensure_directory_exists(self, path: str) -> str:
-        if os.path.exists(path) and not os.path.isdir(path):
+        if self.path_exists(path) and not self.is_dir(path):
             raise NotADirectoryError(f"Path is not a directory: {path}")
 
         os.makedirs(path, exist_ok=True)
