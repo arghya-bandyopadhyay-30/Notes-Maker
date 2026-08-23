618bce7075e091360fe93f9e746945a1f61e9d37
Arghya Bandyopadhyay <arghya.bandyopadhyay.official1@gmail.com>
2026-08-23 18:04:08 +0530

    Rename LLMConfig.models to models_config
    - Internal field: models -> models_config
    - YAML key: models -> models_config (via MODELS_CONFIG constant)
    - Updated config.yaml to use models_config

--- src/utils/config/llm_config.py
+++ src/utils/config/llm_config.py
@@ -6,7 +6,7 @@ from src.utils.config.models_config import ModelsConfig
 from src.bootstrap.container import DependencyContainer
 from src.utils.naming.resolver import get_llm_provider_class_name
 from src.utils.formatting.strings import (
-    MODELS,
+    MODELS_CONFIG,
     PROVIDER_MODEL,
     VALIDATION_THRESHOLD,
     VALIDATION_THRESHOLD_KEY,
@@ -17,14 +17,14 @@ from src.utils.validation.validators import require_field
 @dataclass
 class LLMConfig:
     provider_model: ProviderModels
-    models: ModelsConfig
+    models_config: ModelsConfig
     provider: LLMProvider
     validation_threshold: float = VALIDATION_THRESHOLD
 
     def to_dict(self) -> dict:
         return {
             PROVIDER_MODEL: self.provider_model.value,
-            MODELS: self.models.to_dict(),
+            MODELS_CONFIG: self.models_config.to_dict(),
             VALIDATION_THRESHOLD_KEY: self.validation_threshold,
         }
 
@@ -36,7 +36,7 @@ class LLMConfig:
 
         return cls(
             provider_model=provider_model,
-            models=ModelsConfig.from_dict(require_field(data, MODELS)),
+            models_config=ModelsConfig.from_dict(require_field(data, MODELS_CONFIG)),
             provider=get_llm_provider_class_name(
                 provider_model_name=provider_model.value,
                 dependencies=dependencies
